07423e6e3ec683067d47a5c69efbd57bb2b75749
ella1011 <37376270+ella1011@users.noreply.github.com>
2018-04-12 09:42:28 +0200

    Update gpu_start0_B_tensorflow.sh

--- gpu_scripts/gpu_start0_B_tensorflow.sh
+++ gpu_scripts/gpu_start0_B_tensorflow.sh
@@ -2,51 +2,69 @@
 
 # TENSORFLOW -> FIRST CUDnn
 
-read -p "chnme was run? " -n 1 -r
-echo "proceeding to cudnn 7.05 install"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+read -p "chnme was run? " answer
+case ${answer:0:1} in
+    y|Y )
+        echo proceeding to cudnn 7.05 install
+        echo running nvd
+        nvd
+        echo running sagi python3-numpy python3-wheel python3-dev python3-pi
+        echo "         "
+        sagi python3-numpy python3-wheel python3-dev python3-pi
+    ;;
+    * )
+        echo Exiting script
+        exit 1
+    ;;
+esac
 
-echo "                "
-echo " running nvd "
-nvd
-echo "sagi python3-numpy python3-wheel python3-dev python3-pip"
-echo "                "
-sagi python3-numpy python3-wheel python3-dev python3-pip
+read -p "All ok? Proceed to install cudnn now? " answer
+case ${answer:0:1} in
+    y|Y )
+        s dpkg -i libcudnn7-doc_7.0.5.15-1+cuda9.0_amd64.deb
+        cd /home/username/gpu_rel/cudnn
+        s dpkg -i libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb
+    ;;
+    * )
+        echo Exiting script
+        exit 1
+    ;;
+esac
 
-fi
-read -p "All ok? Install cudnn now?" -n 1 -r
-echo "                "
-if [[ $REPLY =~ ^[Yy]$ ]]
-thens dpkg -i libcudnn7-doc_7.0.5.15-1+cuda9.0_amd64.deb
+read -p "All ok? Proceed to install cudnn-DEV now? " answer
+case ${answer:0:1} in
+    y|Y )
+        s dpkg -i libcudnn7-dev_7.0.5.15-1+cuda9.0_amd64.deb
+    ;;
+    * )
+        echo Exiting script
+        exit 1
+    ;;
+esac
 
-cd /home/username/gpu_rel/cudnn
-s dpkg -i libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb
-fi
+read -p "All ok? Proceed to install cudnn-DOC now? " answer
+case ${answer:0:1} in
+    y|Y )
+        s dpkg -i libcudnn7-doc_7.0.5.15-1+cuda9.0_amd64.deb
+    ;;
+    * )
+        echo Exiting script
+        exit 1
+    ;;
+esac
 
-read -p "All ok? Install cudnn dev now?" -n 1 -r
-echo "                "
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-s dpkg -i libcudnn7-dev_7.0.5.15-1+cuda9.0_amd64.deb
-fi
-
-read -p "All ok? Install cudnn doc now?" -n 1 -r
-echo "                "
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-s dpkg -i libcudnn7-doc_7.0.5.15-1+cuda9.0_amd64.deb
-fi
-
-read -p "If all is ok, proceeding to verify cudnn installation?" -n 1 -r
-echo "                "
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-s cp -r /usr/src/cudnn_samples_v7/ $HOME
-cd $HOME/cudnn_samples_v7/mnistCUDNN
-make clean
-make
-./mnistCUDNN
-echo "If succeeded, proceed to the last script"
-echo "                "
-fi
+read -p "If all is ok, proceeding to verify cudnn installation? " answer
+case ${answer:0:1} in
+    y|Y )
+        s cp -r /usr/src/cudnn_samples_v7/ $HOME
+        cd $HOME/cudnn_samples_v7/mnistCUDNN
+        make clean
+        make
+        ./mnistCUDNN
+        echo "If succeeded, proceed to the last script"
+        echo "                "    ;;
+    * )
+        echo Exiting script
+        exit 1
+    ;;
+esac
