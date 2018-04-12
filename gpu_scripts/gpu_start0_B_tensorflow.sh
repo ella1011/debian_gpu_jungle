4e675b70d1ea5480498827f840276fa64a382046
ella1011 <37376270+ella1011@users.noreply.github.com>
2018-04-12 09:55:22 +0200

    Update gpu_start0_B_tensorflow.sh

--- gpu_scripts/gpu_start0_B_tensorflow.sh
+++ gpu_scripts/gpu_start0_B_tensorflow.sh
@@ -5,15 +5,15 @@
 read -p "chnme was run? " answer
 case ${answer:0:1} in
     y|Y )
-        echo proceeding to cudnn 7.05 install
-        echo running nvd
+        echo "proceeding to cudnn 7.05 install"
+        echo "running nvd"
         nvd
-        echo running sagi python3-numpy python3-wheel python3-dev python3-pi
+        echo "running sagi python3-numpy python3-wheel python3-dev python3-pi"
         echo "         "
         sagi python3-numpy python3-wheel python3-dev python3-pi
     ;;
     * )
-        echo Exiting script
+        echo "Exiting script"
         exit 1
     ;;
 esac
@@ -21,12 +21,16 @@ esac
 read -p "All ok? Proceed to install cudnn now? " answer
 case ${answer:0:1} in
     y|Y )
+        echo "Running:"      
+        echo "s dpkg -i libcudnn7-doc_7.0.5.15-1+cuda9.0_amd64.deb"
+        echo "cd /home/username/gpu_rel/cudnn"
+        echo "s dpkg -i libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb"
         s dpkg -i libcudnn7-doc_7.0.5.15-1+cuda9.0_amd64.deb
         cd /home/username/gpu_rel/cudnn
         s dpkg -i libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb
     ;;
     * )
-        echo Exiting script
+        echo "Exiting script"
         exit 1
     ;;
 esac
@@ -34,10 +38,12 @@ esac
 read -p "All ok? Proceed to install cudnn-DEV now? " answer
 case ${answer:0:1} in
     y|Y )
+        echo "s dpkg -i libcudnn7-dev_7.0.5.15-1+cuda9.0_amd64.deb"
         s dpkg -i libcudnn7-dev_7.0.5.15-1+cuda9.0_amd64.deb
+
     ;;
     * )
-        echo Exiting script
+        echo "Exiting script"
         exit 1
     ;;
 esac
@@ -45,10 +51,12 @@ esac
 read -p "All ok? Proceed to install cudnn-DOC now? " answer
 case ${answer:0:1} in
     y|Y )
+        echo "Running:"      
+        echo "s dpkg -i libcudnn7-doc_7.0.5.15-1+cuda9.0_amd64.deb"
         s dpkg -i libcudnn7-doc_7.0.5.15-1+cuda9.0_amd64.deb
     ;;
     * )
-        echo Exiting script
+        echo "Exiting script"
         exit 1
     ;;
 esac
@@ -56,6 +64,7 @@ esac
 read -p "If all is ok, proceeding to verify cudnn installation? " answer
 case ${answer:0:1} in
     y|Y )
+        echo "verifying: mnistCUDNN"
         s cp -r /usr/src/cudnn_samples_v7/ $HOME
         cd $HOME/cudnn_samples_v7/mnistCUDNN
         make clean
@@ -64,7 +73,7 @@ case ${answer:0:1} in
         echo "If succeeded, proceed to the last script"
         echo "                "    ;;
     * )
-        echo Exiting script
+        echo "Exiting script"
         exit 1
     ;;
 esac
