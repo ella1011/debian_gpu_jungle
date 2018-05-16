0ac1085047ce59bcccb1bccc8530e16dfd25e660
ella1011 <37376270+ella1011@users.noreply.github.com>
2018-05-16 14:47:39 +0200

    Update gpu_start0_A_nvidiaCuda.sh

--- gpu_scripts/gpu_start0_A_nvidiaCuda.sh
+++ gpu_scripts/gpu_start0_A_nvidiaCuda.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 echo "installing linux headers"
+sleep 2
 sagi linux-headers-$(uname -r)
 echo "            "
 echo "copying confs & updating initramfs"
+sleep 2
 echo "            "
 s cp ~/confs/blacklist-nouveau.conf /etc/modprobe.d/
 s update-initramfs -u
 echo "            "
 echo "starting cuda installer #1"
+sleep 2
+echo "            "
 s sh cuda_9.0.176_384.81_linux.run
 echo "            "
 echo "starting cuda installer #2"
+sleep 2
+echo "            "
 s sh cuda_9.0.176.1_linux.run
 echo "            "
 echo "And some more conf-things: "
+sleep 2
 echo "            "
 s mkdir /usr/lib/systemd/system
 s cp -f ~/confs/nvidia-persistenced.service /usr/lib/systemd/system/
@@ -31,4 +38,4 @@ case ${answer:0:1} in
         exit 1
     ;;
 esac
-
+sleep 2
