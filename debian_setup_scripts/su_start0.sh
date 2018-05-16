c9469296f32b2f058cbac7597c62d9928c921a96
ella1011 <37376270+ella1011@users.noreply.github.com>
2018-05-16 14:46:17 +0200

    Update su_start0.sh

--- debian_setup_scripts/su_start0.sh
+++ debian_setup_scripts/su_start0.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
 echo "apt-get install sudo"
+sleep 2
 apt-get install sudo
 
 echo "adduser myuser sudo"
+sleep 2
 adduser myuser sudo
 
 echo "cp -r /mnt/confs /home/myuser/"
+sleep 2
 cp -r /mnt/confs /home/myuser/
 
 echo "cp -r /mnt/gpu_rel /home/myuser/"
+sleep 2
 cp -r /mnt/gpu_rel /home/myuser/
 
 echo "cp -r /mnt/sys_start0.sh /home/myuser/"
+sleep 2
 cp -r /mnt/sys_start0.sh /home/myuser/
 
 echo "chmod 755 /home/myuser/gpu_rel/gpu_*.sh"
+sleep 2
 chmod 755 /home/myuser/gpu_rel/gpu_*.sh
 
 echo "chmod 755 /home/myuser/sys_*.sh"
+sleep 2
 chmod 755 /home/myuser/sys_*.sh
 
 echo "reboot"
+sleep 2
 reboot
